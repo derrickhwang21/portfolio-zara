afef7a19bc93977af69ae3333ba2c5bc4f4b8724
dhwang21 <hwang.derrick@gmail.com>
2018-10-21 14:17:33 -0700

    prove of life for server side rendering

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-var http = require("http");
+'use strict';
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("It's alive!");
-  response.end();
-}).listen(3000);
+const express = require('express');
+const superagent = require('superagent');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', pageLoad);
+
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+function pageLoad(request, response) {
+  response.render('index')
+}
