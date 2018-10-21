027a08fc36b64390922b9d2e425d6e657792cd0d
dhwang21 <hwang.derrick@gmail.com>
2018-10-21 14:32:28 -0700

    added jquery

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 
 app.get('/', pageLoad);
 
