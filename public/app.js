33e1c501b02461c93ee7fe0b5bd86f0803f6a0b9
dhwang21 <hwang.derrick@gmail.com>
2018-10-30 15:57:17 -0700

    working on spacing

--- public/app.js
+++ public/app.js
@@ -1,11 +1,1 @@
-// Animations init
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("scrollHide").style.backgroundColor = "transparent";
-  } else {
-    document.getElementById("scrollHide").style.backgroundColor = "gray";
-  }
-  prevScrollpos = currentScrollPos;
-}
+new WOW().init();
